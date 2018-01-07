dd21c335277671f85935757e88ed3134e1e067cc
José Maillard <jose.maillard@ensimag.fr>
2018-01-07 15:50:13 +0100

    redirectio to cafet info to be able to work on cafet interface

--- src/app/cafet/cafet-guard/cafet-user.service.ts
+++ src/app/cafet/cafet-guard/cafet-user.service.ts
@@ -9,8 +9,8 @@ export class CanActivateCafetUser  implements CanActivate {
   constructor(private cafet: CafetService, private router: Router) { }
 
   canActivate(): boolean {
-    // this.router.navigateByUrl("/cafet-info");
-    // return false
-    return true;
+    this.router.navigateByUrl("/cafet-info");
+    return false
+    // return true;
   }
 }
--- src/app/cafet/cafet-info/cafet-info.component.ts
+++ src/app/cafet/cafet-info/cafet-info.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { DicoService } from '../../language/dico.service';
 
 @Component({
   selector: 'app-cafet-info',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CafetInfoComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public d: DicoService
+  ) { }
 
   ngOnInit() {
   }
