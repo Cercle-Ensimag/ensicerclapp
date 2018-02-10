25d8bafadc42b2bc08a3bcc9e67320153e03f8d5
José Maillard <jose.maillard@ensimag.fr>
2018-02-10 18:18:44 +0100

    to lower case to be sure

--- src/app/auth/auth-service/auth.service.ts
+++ src/app/auth/auth-service/auth.service.ts
@@ -202,7 +202,7 @@ export class AuthService {
   }
 
   getEmailIdFromEmail(email: string) {
-    return email.replace('@ensimag.fr', '').replace('.', '|');
+    return email.toLowerCase().replace('@ensimag.fr', '').replace('.', '|');
   }
 
   getUserAccountPath(): string {
