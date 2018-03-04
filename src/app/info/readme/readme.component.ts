600b745c8e5de89b54110f679031e9a1a43a12d3
José Maillard <jose.maillard@ensimag.fr>
2018-03-04 17:04:08 +0100

    update readme with user guide

--- src/app/info/readme/readme.component.ts
+++ src/app/info/readme/readme.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+
+import { DicoService } from '../../language/dico.service';
 
 @Component({
   selector: 'app-readme',
@@ -7,9 +10,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReadmeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private location: Location,
+    public d: DicoService
+  ) { }
 
   ngOnInit() {
+  }
+
+  back() {
+    this.location.back();
   }
 
 }
