62952a5fd9e31a579b841b67d32577138946f5b4
José Maillard <jose.maillard@ensimag.fr>
2020-02-29 18:01:07 +0100

    ensicerclapp is default project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,14 +2,12 @@ export const environment = {
 	production: true,
 	hmr: false,
 	firebase: {
-		apiKey: "AIzaSyCzy9RKUeWhYU0yApT3da5zBh1g6MbVTDU",
-		authDomain: "cercle-ensimag.firebaseapp.com",
-		databaseURL: "https://cercle-ensimag.firebaseio.com",
-		projectId: "cercle-ensimag",
-		storageBucket: "cercle-ensimag.appspot.com",
-		messagingSenderId: "263147966493",
-		appId: "1:263147966493:web:5f48b801710e47b587b6f4",
-		measurementId: "G-D400V199NZ"
+		apiKey: "AIzaSyCiMXiQ2w5b7YWmW20j38PC2mrORsNVHMk",
+		authDomain: "ensicerclapp.firebaseapp.com",
+		databaseURL: "https://ensicerclapp.firebaseio.com",
+		projectId: "ensicerclapp",
+		storageBucket: "ensicerclapp.appspot.com",
+		messagingSenderId: "5656243105"
 	},
 	proxy: {
 		domain: "https://cercle-ensimag.fr/proxy.php?"
